36a68b4f6dccef849508d9848cca939728f42a69
Jessica Wang <1271736670@qq.com>
2026-09-08 21:47:28 +0800

    feat: day2-9.员工分页查询-代码完善

--- sky-take-out-backend/sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-take-out-backend/sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -59,13 +59,11 @@ public class Employee implements Serializable {
     /*
      * 创建时间
      * */
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
     /*
      * 更新时间
      * */
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
     /*
