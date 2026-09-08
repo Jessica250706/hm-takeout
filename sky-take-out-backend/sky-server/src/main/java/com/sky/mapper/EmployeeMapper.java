e1f89e76e840b4acf6a7df084e8427ed1b03aabd
Jessica Wang <1271736670@qq.com>
2026-09-08 14:35:35 +0800

    fix: 修正mapper中错误的sql占位符

--- sky-take-out-backend/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-take-out-backend/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -25,7 +25,7 @@ public interface EmployeeMapper {
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, " +
             "create_time, update_time, create_user, update_user) " +
             "values " +
-            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNUmber}, #{statue}, " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, " +
             "#{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void save(Employee employee);
 }
