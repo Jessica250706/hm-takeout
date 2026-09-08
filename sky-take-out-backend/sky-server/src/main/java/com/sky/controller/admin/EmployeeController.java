1c4293b4fdcb4c728ea6a6f10cf48a4fab0d1657
Jessica Wang <1271736670@qq.com>
2026-09-08 14:20:24 +0800

    feat: day2-2.新增员工-代码开发

--- sky-take-out-backend/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out-backend/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -32,7 +33,7 @@ public class EmployeeController {
     private JwtProperties jwtProperties;
 
     /**
-     * 登录
+     * 员工登录
      *
      * @param employeeLoginDTO
      * @return
@@ -62,12 +63,25 @@ public class EmployeeController {
     }
 
     /**
-     * 退出
+     * 员工退出
      *
      * @return
      */
     @PostMapping("/logout")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工：{}", employeeDTO);
+        employeeService.save(employeeDTO);
         return Result.success();
     }
 
