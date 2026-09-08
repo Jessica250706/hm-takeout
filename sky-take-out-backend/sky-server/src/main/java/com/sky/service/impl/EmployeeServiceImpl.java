c2664d700177f1bdf15a411a82e51600dcbbc0a9
Jessica Wang <1271736670@qq.com>
2026-09-08 15:08:21 +0800

    feat: day2-5.新增员工-代码完善2

--- sky-take-out-backend/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out-backend/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -77,9 +78,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
         // 设置当前记录创建人 id 和修改人 id
-        // TODO: 后期需要修改为当前登录用户的 id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.save(employee);
     }
